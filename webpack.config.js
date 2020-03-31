d1915db6c5439bbd98a9efdc9966e032599251f1
sigma-us <kyleconley2@gmail.com>
2020-03-31 10:48:48 -0700

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
             use: [{
                 loader: 'awesome-typescript-loader',
                 options: {
-                    useBabel: false,
+                    useBabel: true,
                     useCache: true
                 }
             }]
