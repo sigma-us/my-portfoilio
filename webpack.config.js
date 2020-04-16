d836e167b0d9b14c932e784f7151b90f6de0a57c
sigma-us <kyleconley2@gmail.com>
2020-04-16 12:50:00 -0700

    webpack mod, example png change

--- webpack.config.js
+++ webpack.config.js
@@ -39,27 +39,11 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: "source-map-loader"
-        }, {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            use: 'css-loader'
-        }, {
-            test: /\.png$/,
-            exclude: /node_modules/,
-            use: 'raw-loader'
-        }, {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            use: 'html-loader'
         }]
     },
     plugins: [
         new CheckerPlugin(),
         new CleanWebpackPlugin(),
-        // new HtmlWebpackPlugin({
-        //     template: './index.html',
-        //     favicon: './favicon.ico',
-        // }),
     ],
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
