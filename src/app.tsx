521802b43c5cb29492b3aa576778fcc66c355fa7
sigma-us <kyleconley2@gmail.com>
2020-06-20 18:22:09 -0700

    about me and post styles

--- src/app.tsx
+++ src/app.tsx
@@ -39,7 +39,7 @@ const PostContainer = styled.div`
 const Post = styled.div`
     border-radius: 5px 5px 5px 5px;
     position: relative;
-    height: 650px;
+    height: 450px;
     width: 500px;
     margin: 4px;
     background-image: ${params => params.title ? `url("../images/${params.title}.png")` : `url("../images/${params.title}.png")`};
@@ -79,16 +79,23 @@ const Title = styled.div`
 const AboutMeText = styled.div`
     font-weight: 100;
     letter-spacing: 0.7px;
+    line-height: 32px;
     padding: 80px;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 30px;
-    text-align: center;
-    background-color: white;
+    font-size: 24px;
+    text-align: left;
+    background-color: inherit;
 
     div {
         width: 800px;
+        height: fit-content;
+        border: 1px solid rgb(30,33,40);
+        border-radius: 8px;
+        padding: 32px 32px 96px 32px;
+        background-color: rgb(45,45,45);
+        color: rgb(255,255,255);
     }
 
 `
@@ -98,7 +105,7 @@ const AboutMeHeading = styled.div`
     justify-content: center;
     align-items: center;
     background: rgba(30, 33, 40, 1);
-    height: 600px;
+    height: 250px;
     font-size: 70px;
     color: white;
     text-align: center;
@@ -151,10 +158,10 @@ class App extends Component<any, any> {
 
                 <AboutMeText>
                     <div>
-                        I have 3+ years experience in Full-Stack Web Development and Cloud Architecture. I have been using AWS since the beginning and
+                        {`I have 3+ years experience in Full-Stack Web Development and Cloud Architecture. I have been using AWS since the beginning and
                         love to design and build custom solutions to solve problems in the most cost-efficient way possible without sacrificing performance.
                         I have predominantly used Node.js over past 3 years however I am language agnostic and exploring Golang and C++. This website is being
-                        hosted on AWS CloudFront with Route 53 for decreased latency and increased availability.
+                        hosted on AWS CloudFront with Route 53 for decreased latency and increased availability.`}
                     </div>
                 </AboutMeText>
             </AppC>
