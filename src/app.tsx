d51ed64842f4f437a3783ea670e6f9216fdeed74
sigma-us <kyleconley2@gmail.com>
2022-12-20 17:25:56 -0800

    quick update on whats getting worked on

--- src/app.tsx
+++ src/app.tsx
@@ -104,42 +104,42 @@ const Title = styled.div`
     }
 `;
 
-// const AboutMeText = styled.div`
-//     position: relative;
-//     font-weight: 100;
-//     letter-spacing: 0.7px;
-//     line-height: 32px;
-//     padding: 80px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     font-size: 24px;
-//     text-align: left;
-//     background-color: rgb(30,33,40);
-//     z-index: 4;
+const AboutMeText = styled.div`
+    position: relative;
+    font-weight: 100;
+    letter-spacing: 0.7px;
+    line-height: 32px;
+    padding: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    text-align: left;
+    background-color: rgb(30,33,40);
+    z-index: 4;
 
     
-//     div {
-//         width: 800px;
-//         height: fit-content;
-//         border: 1px solid rgb(30,33,40);
-//         border-radius: 8px;
-//         padding: 32px 32px 96px 32px;
-//         background-color: rgb(45,45,45);
-//         color: rgb(255,255,255);
-//         box-shadow: 2px 2px 2px 0px rgb(0,0,0);
-//     }
-//     @media(max-width: 991px) {
-//         font-size: 16px;
-//         line-height: 22px;
-//         padding: 24px;
+    div {
+        width: 800px;
+        height: fit-content;
+        border: 1px solid rgb(30,33,40);
+        border-radius: 8px;
+        padding: 32px 32px 64px 32px;
+        background-color: rgb(45,45,45);
+        color: rgb(255,255,255);
+        box-shadow: 2px 2px 2px 0px rgb(0,0,0);
+    }
+    @media(max-width: 991px) {
+        font-size: 16px;
+        line-height: 22px;
+        padding: 24px;
 
-//         div {
-//             padding: 16px;
-//         }
-//     }
+        div {
+            padding: 16px;
+        }
+    }
 
-// `
+`
 
 const AboutMeHeading = styled.div`
     position: relative;
@@ -148,7 +148,7 @@ const AboutMeHeading = styled.div`
     align-items: center;
     background: rgba(30, 33, 40, 1);
     height: 250px;
-    font-size: 70px;
+    font-size: 48px;
     color: white;
     text-align: center;
     font-weight: 200;
@@ -204,19 +204,25 @@ class App extends Component<any, any> {
                 </PostContainer>
 
                 <AboutMeHeading>
-                    {/* About Me */}
+                    Whats Happening
                 </AboutMeHeading>
 
-                {/* <AboutMeText>
+                <AboutMeText>
                     <div>
-                        {`I have 3+ years experience in Full-Stack Web Development and Cloud Architecture. I have been using AWS since the beginning and
+                        Currently working on upgrading existing infrastructure and CI/CD: 
+                        <br/>- move from AWS Codepipeline to CircleCi 
+                        <br/>- update terraform config to prevent breaking changes with v4
+                        <br/>- launch strapi headless CMS + DevOps automation magic
+                        <br/>- update react app and include GatsbyJs with more magic
+                        <br/>- have fun
+                        {/* {`I have 3+ years experience in Full-Stack Web Development and Cloud Architecture. I have been using AWS since the beginning and
                         love to design and build custom solutions to solve problems in the most cost-efficient way possible without sacrificing performance. (Since building 
                         this site I have come to discover that many frontend javascript frameworks are a bit overkill for many use cases, SSR appears to be ideal for SEO as well as initial page rendering.
                         For businesses and individuals that wish to design for scale I would recommend a blend of SSR with minimal frontend javascript). This website is being
                         hosted on AWS CloudFront with Route 53 for decreased latency and increased availability. There is much I can do to optimize the performance of this website. I am blessed to have an 
-                        employer to keep me busy during these times, so this page will remain as it is for now. I will slowly optimize this site as time permits.`}
+                        employer to keep me busy during these times, so this page will remain as it is for now. I will slowly optimize this site as time permits.`} */}
                     </div>
-                </AboutMeText> */}
+                </AboutMeText>
             </AppC>
         )
     }
